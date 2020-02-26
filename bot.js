995db0237859c34a2a285f817023de9c320ffb0d
Petr Korolev <sky4winder@gmail.com>
2020-02-26 11:01:48 +0700

    deploy tgest

--- bot.js
+++ bot.js
@@ -1,13 +1,17 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 const token = process.env.TOKEN
-console.log(token)
-const bot = new TelegramBot(token, {polling: true});
+l("token: "+ token)
+const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', (msg) => {
+    l(msg)
     const chatId = msg.chat.id;
-
     // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
+    bot.sendMessage(chatId, 'Received your message').then(r => l(r))
 });
 
+function l(obj) {
+    // obj = JSON.stringify(obj, null, 4); // (Optional) beautiful indented output.
+    console.log(obj); // Logs output to dev tools console.
+}
