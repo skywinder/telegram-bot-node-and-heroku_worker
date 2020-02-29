cb1a49cd2a1008f04535803ed9ecffe626b64396
Petr Korolev <sky4winder@gmail.com>
2020-02-29 15:11:44 +0700

    refactoring

--- bot.js
+++ bot.js
@@ -1,18 +1,32 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 const token = process.env.TOKEN;
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', (msg) => {
     l(msg);
     const chatId = msg.chat.id;
-    var d = new Date().toLocaleTimeString();
-    let obj = "WEB: " + d;
-    l(obj);
-    bot.sendMessage(chatId, obj);
+    var date = new Date().toLocaleTimeString();
+
+    let web = process.env.ENV_TYPE || "UNDEF_TYPE";
+    let str = web + ": " + date + "\nChatId: " + chatId.toString();
+    l(str);
+    bot.sendMessage(chatId, str).then(r =>
+    {
+        bot.sendMessage(chatId, s([str , r.message_id, r.from.id, r.chat.id]));
+    });
+    bot.getMe().then(sucess =>
+    {
+        bot.sendMessage(chatId, s(sucess.id));
+    })
+
 });
 
 function l(obj) {
-    // obj = JSON.stringify(obj, null, 4); // (Optional) beautiful indented output.
+    obj = JSON.stringify(obj, null, 4); // (Optional) beautiful indented output.
     console.log(obj); // Logs output to dev tools console.
+}
+
+function s(obj) {
+    return JSON.stringify(obj, null, 4); // (Optional) beautiful indented output
 }
