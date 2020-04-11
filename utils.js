5d192b728f2ea7eb84796e38b6d51cc0dadff82d
Petr Korolev <sky4winder@gmail.com>
2020-04-11 15:53:59 +0700

    clenaup

--- utils.js
+++ utils.js
@@ -1,6 +1,5 @@
 function l(obj) {
-    obj = JSON.stringify(obj, null, 4); // (Optional) beautiful indented output.
-    console.log(obj); // Logs output to dev tools console.
+        console.log(s(obj)); // Logs output to dev tools console.
 }
 module.exports.l = l;
 function s(obj) {
